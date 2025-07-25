1ba11260f12e21d10eb51652abe35527d5485efa
smhnaghavi <s.m.hadi.naghavi@gmail.com>
2025-07-26 00:39:16 +0330

    Update question 2

--- 2.py
+++ 2.py
@@ -1,23 +1,23 @@
 # Q2
-# Solution in O(n * b)
+# Solution in O(b)
 
 try:
     n = int(input("Enter n: "))
-    cost, magicvalue = [0 for _ in range(n+1)], [0 for _ in range(n+1)]
     cost = list(map(int, input("Enter costs list: ").split()))
-    magicValue = list(map(int, input("Enter magic values: ").split()))
+    magic = list(map(int, input("Enter magic values: ").split()))
     b = int(input("Enter the maximum cost: "))
 except ValueError:
     print("Invalid Input!")
     exit()
 
-dp = [[-1 for _ in range(b+1)] for _ in range(n+1)]
+dp = [-1 for _ in range(b+1)]
+dp[0] = 0
 
 ans = 0
 for i in range(1, n+1):
-    for j in range(1, b+1):
-        if (j - cost[i]) >= 0 and dp[i-1][j-cost[i]] != -1:
-            dp[i][j] = dp[i - 1][j-cost[i]] + magicvalue[i]
-            ans = max(ans, dp[i][j])
+    for j in range(b, cost[i] - 1, -1):
+        if j - cost[i] != -1:
+            dp[j] = max(dp[j], dp[j-cost[i]] + magic[i])
+            ans = max(ans, dp[j])
         
 print(ans)
