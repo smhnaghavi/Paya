e6ea0b5cb2cdfdfd26800c01a64ff76489d036f8
Abolfazl-11 <abolfazl11.dalvi@gmail.com>
2025-07-31 22:58:04 +0330

    update 8

--- 8.py
+++ 8.py
@@ -1,57 +1,57 @@
-from collections import defaultdict
-
 N = 200010
 
-adj = defaultdict(set)
-radj = defaultdict(set)
+adj = [set() for _ in range(N)]
+radj = [set() for _ in range(N)]
 visited = [0] * N
 topol = []
 ok = True
 
 def DFS(v):
-    global ok
+    global ok, topol
     visited[v] = 1
 
     for u in adj[v]:
         if not visited[u]:
             DFS(u)
-
+    
     for u in radj[v]:
         if visited[u] > 1:
             ok = False
-
+    
     visited[v] += 1
     topol.append(v)
 
 n, m, q = map(int, input().split())
 
-for i in range(m):
+for _ in range(m):
     u, v = map(int, input().split())
     adj[u].add(v)
     radj[v].add(u)
 
-for i in range(q):
-    t, u, v = input().split()
-    u, v = int(u), int(v)
-
+for _ in range(q):
+    parts = input().split()
+    t = parts[0]
+    u, v = map(int, parts[1:])
+    
     if t == '+':
         adj[u].add(v)
         radj[v].add(u)
     else:
-        adj[u].discard(v)
-        radj[v].discard(u)
-
-    visited[:] = [0] * (n + 1)
-    topol.clear()
+        if v in adj[u]:
+            adj[u].remove(v)
+        if u in radj[v]:
+            radj[v].remove(u)
+    
+    visited = [0] * (n + 1)
+    topol = []
     ok = True
-
+    
     for i in range(1, n + 1):
         if not visited[i]:
             DFS(i)
-
+    
     if ok:
         print("Yes")
-        print(' '.join(map(str, topol[::-1])))
+        print(' '.join(map(str, reversed(topol))))
     else:
         print("No")
-
