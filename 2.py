9b3f65ebc353c0ae19457a1ab65fe75ed43f26d7
smhnaghavi <s.m.hadi.naghavi@gmail.com>
2025-08-01 17:21:53 +0330

    Debug and Update 2

--- 2.py
+++ 2.py
@@ -1,22 +1,34 @@
-n = int(input("Enter number of vertex: "))
-m = int(input("Enter number of edges: "))
+n, m = map(int, input("Enter the number of vertices and the number of edges: ").split())
 
-for i in range(1 , m+1):
-    v = int(input())
-    u = int(input())
+cost, magic, edges = [-1, ], [-1, ], [-1, ]
 
-cost = list(map(int, input("Enter costs list of edges in order: ").split()))
-magic = list(map(int, input("Enter magic values of edges in order: ").split()))
+print("Enter edges u v cost magic: ")
+
+for i in range(m):
+    u, v, c, mag = map(int, input().split())
+    cost.append(c)
+    magic.append(mag)
+    edges.append([u, v])
+
 b = int(input("Enter the maximum cost: "))
 
-dp = [-1 for _ in range(b+1)]
-dp[0] = 0
+dp = [[0 for j in range(b+1)] for i in range(m+1)]
+par = [[0 for j in range(b+1)] for i in range(m+1)]
 
-ans = 0
-for i in range(1, n+1):
-    for j in range(b, cost[i]-1, -1):
-        if j - cost[i] != -1:
-            dp[j] = max(dp[j], dp[j-cost[i]] + magic[i])
-            ans = max(ans, dp[j])
+for i in range(1, m+1):
+    for j in range(b+1):
+        dp[i][j] = dp[i-1][j]
+        par[i][j] = False
+        if j >= cost[i] and dp[i-1][j-cost[i]] + magic[i] > dp[i][j]:
+            dp[i][j] = dp[i-1][j-cost[i]] + magic[i]
+            par[i][j] = True
+
+ans = []
+idx = b - 1
+
+for i in range(m, -1, -1):
+    if par[i][idx]:
+        idx -= cost[i]
+        ans.append(edges[i])
         
 print(ans)
