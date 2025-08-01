da359f35ebcd4e74367ab7e3385ce9c4f544970e
smhnaghavi <s.m.hadi.naghavi@gmail.com>
2025-08-01 19:19:33 +0330

    Update

--- 6.py
+++ 6.py
@@ -1,16 +1,17 @@
 def divide(a, b):
     ans = 0
-
-    while a >= b:
-        tmp = b
-        d = 1
-        while (tmp << 1) <= a:
-            tmp <<= 1
-            d <<= 1
-        a -= tmp
-        ans |= d
-
+    tmp = b
+    cnt = 1
+    while (tmp << 1) <= a:
+        tmp <<= 1
+        cnt <<= 1
+    while tmp >= b:
+        if a >= tmp:
+            a -= tmp
+            ans += cnt
+        tmp >>= 1
+        cnt >>= 1
     return ans
 
-a, b = map(int, input().split())
+a, b = map(int, input("Enter 2 numbers a & b: ").split())
 print(divide(a, b))
